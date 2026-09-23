4f9c35dabca9ee8a4c1205564c62713ab48cd956
Cristobal Cabezas <cscabezase@gmail.com>
2026-09-22 22:44:09 -0300

    Define routes for routes and delivery events

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get '/routes', to: 'routes#index'
+  get '/routes/:id', to: 'routes#show'
+  post '/routes', to: 'routes#create'
+  get '/delivery_events', to: 'delivery_events#index'
+  post '/delivery_events', to: 'delivery_events#create'
 end
